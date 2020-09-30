abc946dcbe8c2288916becdf74a8a2e1f937b403
Carlos Villarreal <carlosavv@utexas.edu>
2020-09-30 11:56:36 -0500

    wip

--- prolate_spheroid.py
+++ prolate_spheroid.py
@@ -6,16 +6,16 @@ from geomdl import convert
 from geomdl import construct
 from geomdl.visualization import VisMPL as vis
 from slice import slice
-plt.style.use('seaborn')
+plt.style.use('dark_background')
 
 def p_spheroid(mu,nu,phi,a = 10):
 	x = a*np.sinh(mu)*np.sin(nu)*np.cos(phi)
 	y = a*np.sinh(mu)*np.sin(nu)*np.sin(phi)
 	z = a*np.cosh(mu)*np.cos(nu)
 	return x,y,z
-mu = 1
-nu = np.linspace(0,np.pi,20)
-phi = np.linspace(0,2*np.pi,20)
+mu = .75
+nu = np.linspace(0,np.pi,10)
+phi = np.linspace(0,2*np.pi,10)
 
 sph = []
 
@@ -25,35 +25,21 @@ for i in range(0,len(nu)):
 
 
 sph = np.array(sph)
+# new_sph = np.delete(sph,np.argmax(sph))
 # sph = np.delete(sph,0)
 # sph = np.delete(sph,len(sph)-1)
+# print(new_sph)
 fig = plt.figure()
 ax = plt.axes(projection = '3d')
-# ax.scatter(sph[:,0],sph[:,1],sph[:,2])
-
-n = 8
-slice(n,sph)
-bins = slice.bins
-
-temp = []
-for i in range(4,8):
-	for j in range(3,7):
-		temp.append([slice.slices[i][:,0],slice.slices[i][:,1],bins[j]*np.ones(len(slice.slices[i][:,2]))])
-temp = np.array(temp)
 layers = []
-for i in range(0,len(temp)):
-	for j in range(0,len(temp[i][0])):
-		layers.append([temp[:,0][i][j],temp[:,1][i][j],temp[:,2][i][j]])
-layers = np.array(layers)
-print(layers)
-
-ax.scatter(layers[:,0],layers[:,1],layers[:,2],color = 'g')
-plt.show()
+for i in range(10,90):
+	layers.append([sph[i,0],sph[i,1],sph[i,2]])
+	ax.scatter(sph[i,0],sph[i,1],sph[i,2])
 print(len(layers))
-
+plt.show()
 p_ctrlpts = layers
-size_u = 9
-size_v = 20
+size_u = 8
+size_v = 10
 degree_u = 3
 degree_v = 3
 
@@ -71,15 +57,19 @@ plot_extras = [
         points=surf_curves['u'][0].evalpts,
         name="u",
         color="red",
-        size=10
+        size=5
     ),
     dict(
         points=surf_curves['v'][0].evalpts,
         name="v",
         color="black",
-        size=10
+        size=5
     )
 ]
 surf.delta = 0.025
-surf.vis = vis.VisSurface()
+surf.vis = vis.VisSurfWireframe()
 surf.render(extras=plot_extras)
+
+evalpts = np.array(surf.evalpts)
+
+print(surf.derivatives(0,0)[0][0])
